534dceba252cf54d36894fd9bb12c0a48051aa77
Tharak Nadh <tharakanadh.battula@gmail.com>
2024-11-13 23:28:39 +0530

    I have modified the profile page

--- src/user/profile/Profile.js
+++ src/user/profile/Profile.js
@@ -1,7 +1,8 @@
 import React from "react";
+import teja from "../../assert/images/Teja.jpg"
 
-const Profile =() => {
-    return(
+const Profile = () => {
+    return (
         <React.Fragment>
             <section className="registration-section p-3">
                 <div className="container">
@@ -12,6 +13,27 @@ const Profile =() => {
                     </div>
                 </div>
             </section>
+            <div className="container mt-3">
+                <div className="row ">
+                    <div className="col-md-4">
+                        <div className="card">
+                            <div className="card-header registration-section">
+                                <img src={teja} alt="Teja" className="img-fluid" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-4">
+                        <div className="card">
+                            <div className="card-body registration-section">
+                                <h3 className="card-title">Teja.P</h3>
+                                <p>Salesforce Developer</p>
+                                <p>Hyderabad</p>
+                                <p>80000.0</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </React.Fragment>
     );
 };
